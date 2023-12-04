fe04ce0f831afb52cfacf661f3bf31d99dedf99e
赵春鹏 <zhaocp@dongruist.com>
2023-12-04 18:54:43 +0800

    测试代码推送

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,13 +2,14 @@
  * @Description:  
  * @Author: 赵春鹏 605252879@qq.com
  * @Date: 2023-09-21 10:41:18
- * @LastEditors: 赵春鹏 605252879@qq.com
- * @LastEditTime: 2023-10-27 15:03:35
- * @FilePath: /my-vue-app/src/router/index.ts
+ * @LastEditors: 赵春鹏 zhaocp@dongruist.com
+ * @LastEditTime: 2023-12-04 18:54:25
+ * @FilePath: \my-vue-app\src\router\index.ts
  * Copyright (c) 2023 by ${git_name} email: ${git_email}, All Rights Reserved.
  */
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router';
 const routes: Array<RouteRecordRaw> = [
+    // 测试推送
     {
         path: '/',
         component: () => import('../components/TableDataList.vue'),
